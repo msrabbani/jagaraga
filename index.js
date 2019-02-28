d0cc75e2df964b64ac2ef379edf583ebef3a5b3d
msrabbani <msrabbanii@gmail.com>
2019-03-01 00:28:57 +0700

    welcome component

--- index.js
+++ index.js
@@ -2,9 +2,11 @@
  * @format
  * @lint-ignore-every XPLATJSCOPYRIGHT1
  */
-
+import React from 'react';
 import {AppRegistry} from 'react-native';
-import App from './App';
 import {name as appName} from './app.json';
+import {Welcome} from './src/containers/Welcome';
 
-AppRegistry.registerComponent(appName, () => App);
+const Root = () => <Welcome />;
+
+AppRegistry.registerComponent(appName, () => Root);
