ffe2601a31e1be119ad7267657a9ba42c1511a66
msrabbani <msrabbanii@gmail.com>
2019-03-05 22:11:16 +0700

    gradient main

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -1,11 +1,14 @@
 import React, {Component} from 'react';
 import {Text, View} from 'react-native';
 import TabView from 'react-native-scrollable-tab-view';
+import LinearGradient from 'react-native-linear-gradient'
 
 export class Main extends Component {
   render() {
     return (
-      <View style={{flex: 1}}>
+        <LinearGradient 
+            colors={['#880E4F','#4A148C']}
+            style={{flex: 1}}>
         <TabView
           tabBarTextStyle={{fontSize: 30}}
           tabBarPosition="overlayBottom"
@@ -13,7 +16,7 @@ export class Main extends Component {
           <Text tabLabel="halo">halo</Text>
           <Text tabLabel="uhuy">uhuy</Text>
         </TabView>
-      </View>
+      </LinearGradient>
     );
   }
 }
