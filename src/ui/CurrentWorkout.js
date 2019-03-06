cf9ecd09632f622cf38c7c8dda2fcd3ee466080f
msrabbani <msrabbanii@gmail.com>
2019-03-06 12:29:00 +0700

    reusabe UI component

--- src/ui/CurrentWorkout.js
+++ src/ui/CurrentWorkout.js
@@ -1,9 +1,27 @@
 import React from 'react'
-import {View, Text} from 'react-native'
+import {View, Text, StyleSheet} from 'react-native'
+import {Topbar} from './Topbar'
 
 export const CurrentWorkout = () => (
     <View>
-        <Text>CurrentWorkout...</Text>
-    </View>
+    <Topbar style={styles.topbar}>
+        <Text style={styles.textbar}>Current Workout</Text>
+    </Topbar>
 
+    <Text>Current Workout...</Text>
+    </View>
 )
+
+const styles = StyleSheet.create({
+    topbar: {
+        backgroundColor: 'transparent',
+        justifyContent: 'center',
+        alignItems: 'center',
+        borderBottomWidth: 1,
+        borderBottomColor: 'white' 
+    },
+    textbar:{
+        color: 'white',
+        fontSize: 25 
+    }
+})
