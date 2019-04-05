db883274610b7766532a03e15857fc720d832be1
msrabbani <msrabbanii@gmail.com>
2019-04-05 21:35:57 +0700

    create api method func

---
+++ src/services/api.js
@@ -0,0 +1,32 @@
+const headers = {
+   'Content-Type': 'application/json',
+   'Accept': 'application/json' 
+}
+
+const checkStatus = (response) => {
+    if(response.status >=200 && response.status < 300) {
+        return response
+    } else {
+        const error = new Error(response.statusText)
+        error.response = response
+        throw error
+    }
+}
+
+const parseJSON = (response) => response.json()
+
+export const get = (path) => (
+    fetch(`http://192.168.1.8:3000/${path}`,{headers})
+    .then(checkStatus)
+    .then(parseJSON)
+)
+
+export const post = (path, data) => (
+    fetch(`http://192.168.1.8:3000/${path}`, {
+        headers,
+        method: 'POST',
+        body: JSON.stringify(data)
+    })
+    .then(checkStatus)
+    .then(parseJSON)
+)
